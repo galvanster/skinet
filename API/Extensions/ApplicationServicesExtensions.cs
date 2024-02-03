72f04d38603c8bc70987fa66167ba20c1dd1d8e8
Carlos Galvan <galvanster@gmail.com>
2024-02-03 00:57:26 -0600

    end of section 15

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -22,6 +22,7 @@ namespace API.Extensions
             });
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ITokenService, TokenService>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
 
             services.Configure<ApiBehaviorOptions>(options =>
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
 
+using Core.Entities.Identity;
 using Core.Interfaces;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
            builder.Services.AddAutoMapper(typeof(MappingProfiles));
            builder.Services.AddControllers();
            builder.Services.AddApplicationServices(builder.Configuration);
+           builder.Services.AddIdentityServices(builder.Configuration);
            builder.Services.AddDbContext<StoreContext>(x =>
              x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+           builder.Services.AddDbContext<AppIdentityDbContext>(x =>
+             x.UseSqlite(builder.Configuration.GetConnectionString("IdentityConnection")));
 
             // builder.Services.AddApplicationServices();
              builder.Services.AddSwaggerDocumentation();
@@ -29,18 +35,24 @@ var builder = WebApplication.CreateBuilder(args);
             app.UseRouting();
             app.UseStaticFiles();
             app.UseCors("CorsPolicy");
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
             app.MapControllers();
          
             var scope = app.Services.CreateScope();
             var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<StoreContext>();
+            var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+            var userManager = services.GetRequiredService<UserManager<AppUser>>();
             var loggerFactory =  services.GetRequiredService<ILoggerFactory>();
             try
             {
-                var context = services.GetRequiredService<StoreContext>();
+
                 await context.Database.MigrateAsync();
+                await identityContext.Database.MigrateAsync();
                 await StoreContextSeed.SeedAsync(context, loggerFactory);
+                await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
             }
             catch (Exception ex)
             {
